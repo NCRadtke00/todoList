e76424c67712c4fa0718fce96bf4bda175877e27
NCRadtke00 <raddnc@gmail.com>
2021-07-05 13:49:48 -0500

    finished adding components with styling, will need to go back and fix the tasktodolist

--- src/components/TaskTodo/TaskInput/TaskInput.js
+++ src/components/TaskTodo/TaskInput/TaskInput.js
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Button from '../../UI/Button/Button';
 
-function TaskInput() {
+import styled from 'styled-components'
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+& label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+  color: ${props => props.invalid ? "red": 'black'};
+}
+& input {
+  display: block;
+  width: 100%;
+  border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+  background: ${props => props.invalid ? '#ffd7d7' :'transparent'};
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+}
+& input:focus {
+  outline: none;
+  background: #f7f3f5;
+  border-color: #8b005d;
+}
+`;
+const TaskInput = props => {
+    const [enteredValue, setEnteredValue] = useState('');
+    const [isValid, setIsValid] = useState(true);
     return (
         <div>
             
